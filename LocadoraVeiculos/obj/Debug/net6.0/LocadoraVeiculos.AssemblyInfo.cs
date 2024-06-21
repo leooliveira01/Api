cf555ad3f86ae63e23efc28803590960c2c00142
leonardooliveira <leonardoaraujooliveira02@gmail.com>
2024-06-21 00:04:37 -0300

    Adicionando funcionalidade

--- LocadoraVeiculos/obj/Debug/net6.0/LocadoraVeiculos.AssemblyInfo.cs
+++ LocadoraVeiculos/obj/Debug/net6.0/LocadoraVeiculos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LocadoraVeiculos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dace578533361bd3bdea801be7916f4ab4fd306")]
 [assembly: System.Reflection.AssemblyProductAttribute("LocadoraVeiculos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LocadoraVeiculos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
